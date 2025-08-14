76bd4df11e41f88e7e9649351fb615bf44c25354
Hossaini1414 <royarahyab53@gmail.com>
2025-08-14 05:56:28 -0700

    new commit

---
+++ resources/views/layouts/Footer.blade.php
@@ -0,0 +1,61 @@
+<!-- Footer  -->
+<div class="bg-[#1e6961] mt-24 w-full text-white" >
+    <div class="flex flex-col-reverse md:flex-row md:gap-12">
+        <!-- Left Section (Brand Description & Socials) -->
+        <div class=" flex flex-col  p-5 w-full md:w-1/3 md:ml-8 text-center">
+            <div class="bg-white h-auto rounded-lg md:rounded-xl px-12 py-1">
+                <h1 class="text-black text-3xl sm:text-2xl lg:text-4xl md:text-4xl text-center p-2">AQSA GROUP</h1>
+            </div>
+            <p class=" text-justify mt-2 text-sm md:text-md">
+                AQSA Group is a leading provider of innovative IT solutions, dedicated to empowering businesses though modern
+                technology and expert support. With a team of professionals in design, development, and digital strategy, we
+                deliver tailored Services that help clients grow, adapt, and succeed in the digital world.
+            </p>
+        </div>
+        <!-- Right Section (Info Columns) -->
+        <div class="w-full md:w-1/2 ml-8 flex text-center md:text-left">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10 p-5 sm:p-8 md:p-10">
+                <!-- Services -->
+                <div class="">
+                    <h1 class="text-lg font-bold mb-3">Services</h1>
+                    <ul class="space-y-2 text-sm ">
+                        <li class="hover:text-blue-500 cursor-pointer underline">Web Development</li>
+                        <li class="hover:text-blue-500 cursor-pointer">Software Development</li>
+                        <li class="hover:text-blue-500 cursor-pointer">Mobile Development</li>
+                        <li class="hover:text-blue-500 cursor-pointer">IT Infrastructure</li>
+                    </ul>
+                </div>
+                <!--  Links -->
+                <div class="cursor-pointer">
+                    <h1 class="text-lg font-bold mb-3">Company</h1>
+                    <ul class="space-y-2 text-sm">
+                        <li class="hover:text-blue-500 cursor-pointer">Achievements</li>
+                        <li class="hover:text-blue-500 cursor-pointer">About Us</li>
+                        <li class="hover:text-blue-500 cursor-pointer">Partners</li>
+                        <li class="hover:text-blue-500 cursor-pointer">Carriers</li>
+                    </ul>
+                </div>
+                <!-- Contact Info -->
+                <div class="flex flex-col gap-3 cursor-pointer text-center md:text-left">
+                    <h1 class="text-lg font-bold ">Contact Us</h1>
+                    <div class="  text-sm hover:text-blue-500 cursor-pointer">
+                        <i class="fas fa-phone text-white"></i>
+                        <span>0797050890</span>
+                    </div>
+                    <div class=" text-sm hover:text-blue-500 cursor-pointer">
+                        <i class="fas fa-phone text-white"></i>
+                        <span>0797050890</span>
+                    </div>
+                    <div class="  text-sm hover:text-blue-500 cursor-pointer" >
+                        <i class="fas fa-envelope text-white"></i>
+                        <span>Info@aqsagroup.af</span>
+                    </div>
+                    <div class="text-sm hover:text-blue-500 cursor-pointer" >
+                        <i class="fas fa-location-dot text-white"></i>
+                        <span>Imam Muslim 22, 64 Meter Street, House #23</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
