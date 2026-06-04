ff74caf6034e507a2a29b3c96863a38dc7bbd646
safiullah63 <safiullahkarimi.dev@gmail.com>
2026-06-04 17:37:29 +0430

    wh added

--- resources/views/userinformation.blade.php
+++ resources/views/userinformation.blade.php
@@ -1,3 +1,19 @@
-<script src="https://cdn.tailwindcss.com"></script>
-<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-@livewire('whatsapp.user-informations')
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    @livewireStyles
+</head>
+<body>
+
+    @livewire('whatsapp.user-informations')
+
+    @livewireScripts
+
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</body>
+</html>
